68fd3010efbf1adcb22ee275dd7bce32662de5ab
NFalone <qoo19830526@gmail.com>
2022-03-03 22:14:41 +0800

    finish basic fetch in home.js new php file

--- PSI/home.js
+++ PSI/home.js
@@ -1,3 +1,15 @@
 let showall = () => {
-    return console.log("test");
+    fetch('https://mtdl.ddns.net/PSI/core.php', {method: 'get'})
+    .then(response => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.text();
+    })
+    .then(restext => {
+        console.log(restext);
+        let testing = document.querySelector("#test");
+        testing.innerHTML = restext;
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
