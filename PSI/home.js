cbafd5374535e6291af6569535eb00752b7b35f9
NFalone <qoo19830526@gmail.com>
2022-03-06 18:34:13 +0800

    create table in sql php with SQL ajax to show sql
    A problem : chinese encode

--- PSI/home.js
+++ PSI/home.js
@@ -1,15 +1,24 @@
 let showall = () => {
-    fetch('https://mtdl.ddns.net/PSI/core.php', {method: 'get'})
+    fetch('https://mtdl.ddns.net/PSI/core.php', {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "title": "showall"
+        })
+    })
     .then(response => {
         if (!response.ok) throw new Error(response.statusText);
-        return response.text();
+        return response.json();
     })
     .then(restext => {
         console.log(restext);
         let testing = document.querySelector("#test");
-        testing.innerHTML = restext;
+        testing.innerHTML = restext[0].year+" "+restext[0].month+" "+restext[0].day+" "+restext[0].sn+" "+restext[0].goods+" "+restext[0].price+" "+restext[0].quantity;
+        // testing.innerHTML = restext;
     })
     .catch(error => {
-        console.log(error);
+        console.log("Error:" + error);
     })
 }
